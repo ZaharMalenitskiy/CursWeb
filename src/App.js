715a01337851cd9cab91bfb3c0882d168fa8378c
TLTahoe <nikosworld@yandex.ru>
2024-05-04 00:57:15 +0300

    EmailRemaster

--- src/App.js
+++ src/App.js
@@ -1,97 +1,172 @@
 import React, { useState } from "react";
 import Popup from "./Popup";
+import DatePicker from "./DatePicker";
 import "./App.css";
 
 function App() {
-  // Состояние для отслеживания текущего шага регистрации
   const [step, setStep] = useState(1);
-  // Состояние для хранения значения логина
   const [loginValue, setLoginValue] = useState("");
-  // Состояние для хранения значения логина
   const [passwordValue, setPasswordValue] = useState("");
-  // Состояние для отображения всплывающего окна
+  const [emailValue, setEmailValue] = useState("");
+  const [genderValue, setGenderValue] = useState("");
   const [showLoginPopup, setShowLoginPopup] = useState(false);
-  // Состояние для отображения всплывающего окна
   const [showPasswordPopup, setShowPasswordPopup] = useState(false);
+  const [showRegisterPopup, setshowRegisterPopup] = useState(false);
+  const [showEmailPopup, setshowEmailPopup] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(true);
 
-  // Запрет ввода пробелов в поля ввода
+  const handleInputChange = (setState) => (e) => {
+    setState(e.target.value);
+  };
+
   const handleInputKeyDown = (e) => {
     if (e.key === " ") {
       e.preventDefault();
     }
   };
 
-  // Функция для обновления значения логина и проверки валидности
-  const handleLoginChange = (e) => {
-    setLoginValue(e.target.value);
-  };
-  // Функция для обновления значения логина и проверки валидности
-  const handlePasswordChange = (e) => {
-    setPasswordValue(e.target.value);
-  };
-
-  // Функция для перехода на следующий шаг
   const goToNextStep = () => {
-    // Проверяем, введен ли логин
-    if (loginValue.trim() !== "") {
-      document.getElementById("username").innerText = document.getElementById("login-input").value
+    const value = loginValue.trim();
+    if (value !== "") {
       setStep(2);
+      document.getElementById("username").innerText = value;
     } else {
-      setShowLoginPopup(true); // Показываем всплывающее окно при пустом логине
+      setShowLoginPopup(true);
       setTimeout(() => setShowLoginPopup(false), 1000);
     }
   };
 
-  const checkPassword = () => {
-    if (passwordValue.trim() !== "") {
-      alert("DONE!");
-    } else {
-      setShowPasswordPopup(true); // Показываем всплывающее окно при пустом логине
-      setTimeout(() => setShowPasswordPopup(false), 1000);
-    }
-  };
-
-  // Функция для перехода на предыдущий шаг
   const goToPreviousStep = () => {
     setStep(1);
   };
 
-  // Функция для закрытия всплывающего окна
+  const checkPassword = () => {
+    const value = passwordValue.trim();
+    if (value !== "") {
+      alert("DONE!");
+    } else {
+      setShowPasswordPopup(true);
+      setTimeout(() => setShowPasswordPopup(false), 1000);
+    }
+  };
+
+  const checkRegistrationData = () => {
+    if (loginValue.trim() === "" || passwordValue.trim() === "" || emailValue.trim() === "" || genderValue.trim() === "") {
+      setshowRegisterPopup(true);
+      setTimeout(() => setshowRegisterPopup(false), 1000);
+    }
+    else if (!validateEmail(emailValue.trim())) {
+      setIsValidEmail(false);
+    }
+    else {
+      setIsValidEmail(true);
+      alert("DONE!")
+    }
+  };
+
+  const checkEmail = () => {
+    if (emailValue.trim() === "") {
+      setshowEmailPopup(true);
+      setTimeout(() => setshowEmailPopup(false), 1000);
+    }
+    else if (!validateEmail(emailValue.trim())) {
+      setIsValidEmail(false);
+    }
+    else {
+      setIsValidEmail(true);
+      alert("DONE!")
+    }
+  };
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const goToThirdStep = () => {
+    setStep(3);
+  };
+
+  const goToFourthStep = () => {
+    setStep(4);
+  }
+
   const closePopup = () => {
     setShowLoginPopup(false);
+    setShowPasswordPopup(false);
+    setshowRegisterPopup(false);
+    setIsValidEmail(true); // Reset email validation on popup close
   };
 
   return (
     <div className="registration-container">
-      {/* Форма Входа */}
       <div className={`registration-form ${step === 1 ? "fade-in" : "fade-out"}`}>
-        <div className="titlelogin1">Авторизация</div><div className="titlelogin2">MusicForYou</div>
+        <div className="titlelogin1">Авторизация</div>
+        <div className="titlelogin2">MusicForYou</div>
         <div className={`input-container`}>
-          <input id="login-input" type="text" placeholder="Введите логин..." value={loginValue} onChange={handleLoginChange} onKeyDown={handleInputKeyDown} />
+          <input id="login-input" type="text" placeholder="Введите логин..." value={loginValue} onChange={handleInputChange(setLoginValue)} onKeyDown={handleInputKeyDown} />
         </div>
         <button className="loginbtn" type="button" onClick={goToNextStep}>Продолжить</button>
-        <button className="regbtn" type="button">Регистрация</button>
+        <button className="regbtn" type="button" onClick={goToThirdStep}>Регистрация</button>
       </div>
 
-      {/* Форма Регистрации */}
       <div className={`registration-form ${step === 2 ? "fade-in" : "fade-out"}`}>
-        <div className="titlepass1">Авторизация</div><div className="titlepass2">MusicForYou</div>
+        <div className="titlepass1">Авторизация</div>
+        <div className="titlepass2">MusicForYou</div>
         <div className="avatar"></div>
-        <div id="username">UserName</div>
+        <div id="username">{loginValue}</div>
         <div className={`input-container`}>
-          <input id="register-input" type="password" placeholder="Введите пароль..." value={passwordValue} onChange={handlePasswordChange} onKeyDown={handleInputKeyDown} />
+          <input id="register-input" type="password" placeholder="Введите пароль..." value={passwordValue} onChange={handleInputChange(setPasswordValue)} onKeyDown={handleInputKeyDown} />
         </div>
-        <button className="submitbtn" type="button" onClick={checkPassword} >Подтвердить вход</button>
+        <button className="submitbtn" type="button" onClick={checkPassword}>Подтвердить вход</button>
         <button id="backbutton" onClick={goToPreviousStep}>Назад</button>
-        <div id="forgot-pass">Забыли пароль?</div>
+        <div id="forgot-pass" onClick={goToFourthStep}>Забыли пароль?</div>
       </div>
 
-      {/* Всплывающее окно */}
+      <div className={`registration-form ${step === 3 ? "fade-in" : "fade-out"}`}>
+        {/* Код для третьего шага */}
+        <div className="titlepass1">Регистрация</div>
+        <div className="titlepass2">MusicForYou</div>
+        <div className={`input-container ${!isValidEmail ? "invalid" : ""}`}>
+          <div>Логин</div>
+          <input type="login" placeholder="Введите логин..." value={loginValue} onChange={handleInputChange(setLoginValue)} onKeyDown={handleInputKeyDown} />
+        </div>
+        <div className={`input-container ${!isValidEmail ? "invalid" : ""}`}>
+          <div>Пароль</div>
+          <input type="pass" placeholder="Введите пароль..." value={passwordValue} onChange={handleInputChange(setPasswordValue)} onKeyDown={handleInputKeyDown} />
+        </div>
+        <div className={`input-container ${!isValidEmail ? "invalid" : ""}`}>
+          <div>EMail</div>
+          <input type="email" placeholder="Введите email..." value={emailValue} onChange={handleInputChange(setEmailValue)} onKeyDown={handleInputKeyDown} />
+        </div>
+        {!isValidEmail && <div className="error-message">Введен некорректный email</div>}
+        <div className={`input-container ${!isValidEmail ? "invalid" : ""}`}>
+          <div>Пол</div>
+          <input type="gender" placeholder="Укажите пол..." value={genderValue} onChange={handleInputChange(setGenderValue)} onKeyDown={handleInputKeyDown} />
+        </div>
+        <DatePicker />
+        <button className="submitbtn" type="button" onClick={checkRegistrationData}>Подтвердить регистрацию</button>
+        <button id="backbutton" onClick={goToPreviousStep}>Назад</button>
+      </div>
+
+      <div className={`registration-form ${step === 4 ? "fade-in" : "fade-out"}`}>
+        <div className="titlepass1">Восстановление пароля</div>
+        <div className="titlepass2">MusicForYou</div>
+        <div className={`input-container ${!isValidEmail ? "invalid" : ""}`}>
+          <div style={{ marginBottom: "1rem" }}>EMail</div>
+          <input type="text" placeholder="Введите email..." value={emailValue} onChange={handleInputChange(setEmailValue)} onKeyDown={handleInputKeyDown} />
+          {!isValidEmail && <div className="error-message">Введен некорректный email</div>}
+        </div>
+        <button className="submitbtn" type="button" onClick={checkEmail}>Отправить</button>
+        <button id="backbutton" onClick={goToPreviousStep}>Назад</button>
+      </div>
+      
+      <Popup message="Пожалуйста, заполните все поля" onClose={closePopup} visible={showRegisterPopup} />
       <Popup message="Пожалуйста, введите логин" onClose={closePopup} visible={showLoginPopup} />
       <Popup message="Пожалуйста, введите пароль" onClose={closePopup} visible={showPasswordPopup} />
+      <Popup message="Пожалуйста, введите email" onClose={closePopup} visible={showEmailPopup} />
     </div>
   );
 }
 
 export default App;
-
